56d98522334fa642c07119123957535bf469a87a
longsiru <sirulong89@gmail.com>
2023-04-17 15:33:22 +0900

    form aggregate

--- app.js
+++ app.js
@@ -1,35 +1,28 @@
 var ArticleModel = require("./model/article.js");
+
 //查询文章信息
+// ArticleModel.find({}, function (err, docs) {
+//   console.log(docs);
+// });
 
-/*
-  ArticleModel.find({},function(err,docs){
-
-    console.log(docs);
-  })
-
-  */
-
-//查询文章信息 并显示文章的分类 以及文章的作者信息
-
+//查询文章信息 并显示文章的分类以及文章的作者信息
 //两个表关联查询
-/*
-    ArticleModel.aggregate([
-
-      {
-
-        $lookup: {
-          from: "articlecate",
-          localField: "cid",
-          foreignField: "_id",
-          as: "cate"
-        }
-      }
-
-    ],function(err,docs){
-
-      console.log(docs[2].cate)
-    })
-  */
+// ArticleModel.aggregate(
+//   [
+//     {
+//       $lookup: {
+//         from: "articlecate",
+//         localField: "cid",
+//         foreignField: "_id",
+//         as: "cate",
+//       },
+//     },
+//   ],
+//   function (err, docs) {
+//     //console.log(JSON.stringify(docs));
+//     console.log(docs[2].cate); //拿到第二个article的cate信息。
+//   }
+// );
 
 //三个表关联查询
 ArticleModel.aggregate(
@@ -45,7 +38,7 @@ ArticleModel.aggregate(
     {
       $lookup: {
         from: "user",
-        localField: "author_id",
+        localField: "author_id", //article里面的author_id要等于user表里的_id相等。
         foreignField: "_id",
         as: "user",
       },
